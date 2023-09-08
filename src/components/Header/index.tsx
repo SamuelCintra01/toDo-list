1fe6bced56252edca293f3fa98e24b385decd881
Samuel Cintra <128873760+SamuelCintra01@users.noreply.github.com>
2023-09-08 11:16:17 -0300

    feat: :sparkles: Funcionalidades:  Adicionar uma nova tarefa

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,33 @@ import todoLogo from "../../assets/Logo.png";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 
 import styles from "./header.module.css";
+import { FormEvent, useState } from "react";
 
-export function Header() {
+interface Prosps {
+  onAddTask: (taskTitle: string) => void;
+}
+
+export function Header({ onAddTask }: Prosps) {
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    onAddTask(title);
+    setTitle("");
+  }
+
   return (
     <header className={styles.header}>
       <img src={todoLogo} alt="" />
 
-      <form className={styles.newTaskForm}>
-        <input type="text" placeholder="Adicione uma nova tarefa" />
+      <form className={styles.newTaskForm} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
         <button>
           Criar
           <AiOutlinePlusCircle size={20} />
